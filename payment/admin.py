b7ecd640a2700fd4b3ebaae728c895093b712430
AlenOl <stoveprofi@gmail.com>
2023-02-27 14:28:03 -0800

    Implemented Payment service

--- payment/admin.py
+++ payment/admin.py
@@ -1,18 +1,4 @@
 from django.contrib import admin
-
 from payment.models import Payment
 
-# class TicketInLine(admin.TabularInline):
-#     model = Ticket
-#     extra = 1
-#
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     inlines = (TicketInLine,)
-
-
 admin.site.register(Payment)
-# admin.site.register(Bus)
-# admin.site.register(Trip)
-# admin.site.register(Ticket)
--- payment/urls.py
+++ payment/urls.py
@@ -1,16 +1,8 @@
-from django.urls import path, include
 from rest_framework import routers
 
-from payment.views import PaymentViewSet
+from .views import PaymentView
 
 app_name = "payment"
 
-
-router = routers.DefaultRouter()
-router.register("payments", PaymentViewSet)
-# router.register("buses", BusViewSet)
-# router.register("trips", TripViewSet)
-# router.register("orders", OrderViewSet)
-# router.register("tickets", TicketViewSet)
-
-urlpatterns = [path("", include(router.urls))]
+router = routers.SimpleRouter()
+router.register("payments", PaymentView, basename="payment")
